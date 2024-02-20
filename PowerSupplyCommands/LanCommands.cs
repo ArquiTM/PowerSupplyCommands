5bb6c43affab0708de688b826a49a125abd16108
Arquimedes Miguel <Arquimedes.Miguel@flex.com>
2024-02-20 09:15:45 -0300

    First commit

---
+++ PowerSupplyCommands/LanCommands.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ivi.Visa.Interop;
+
+namespace PowerSupplyCommands
+{
+    class LanCommands
+    {
+        public LanCommands()
+        {
+
+        }
+
+        public bool SetVoltage()
+        {
+            try
+            {
+                return true;
+
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ PowerSupplyCommands/PowerSupplyGPIBCommands.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using NationalInstruments.VisaNS;
+
+namespace PowerSupplyCommands
+{
+    public class GPIBCommands
+    {
+        public GPIBCommands()
+        {
+
+        }
+        public bool SetVoltage(string addressPowerSupply, double voltage)
+        {
+            try
+            {
+                MessageBasedSession mBs = new MessageBasedSession(addressPowerSupply);
+                mBs.Write("VOLT " + voltage + ";*OPC?");
+                if (Convert.ToInt32(mBs.ReadString()) != 1)
+                    return false;
+
+                return true;
+            }
+            catch { return false; }
+        }
+
+        public double ReadVoltage(string addressPowerSupply)
+        {
+            try
+            {
+                MessageBasedSession mBs = new MessageBasedSession(addressPowerSupply);
+                mBs.Write("MEAS:VOLT?");
+                return Convert.ToInt32(mBs.ReadString());
+            }
+            catch
+            { return -999; }
+        }
+
+        public bool SetMaxCurrent(string addressPowerSupply, double current)
+        {
+            try
+            {
+                MessageBasedSession mBs = new MessageBasedSession(addressPowerSupply);
+                mBs.Write("CURR:LIM:VAL " + current + ";*OPC?");
+                if (Convert.ToInt32(mBs.ReadString()) != 1)
+                    return false;
+
+                return true;
+            }
+            catch
+            { return false; }
+        }
+
+        public double ReadCurrent(string addressPowerSupply)
+        {
+            try
+            {
+                MessageBasedSession mBs = new MessageBasedSession(addressPowerSupply);
+                mBs.Write("MEAS:CURR?");
+                return Convert.ToInt32(mBs.ReadString());
+            }
+            catch { return -999; }
+        }
+
+        public bool OutputOn(string addressPowerSupply)
+        {
+            try
+            {
+                MessageBasedSession mBs = new MessageBasedSession(addressPowerSupply);
+                mBs.Write("OUTP On;*OPC?");
+                if (Convert.ToInt32(mBs.ReadString()) != 1)
+                    return false;
+
+                return true;
+            }
+            catch
+            { return false; }
+        }
+
+        public bool OutputOff(string addressPowerSupply)
+        {
+            try
+            {
+                MessageBasedSession mBs = new MessageBasedSession(addressPowerSupply);
+                mBs.Write("OUTP Off;*OPC?");
+                if (Convert.ToInt32(mBs.ReadString()) != 1)
+                    return false;
+
+                return true;
+            }
+            catch
+            { return false; }
+        }
+    }
+}
